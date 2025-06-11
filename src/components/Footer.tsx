8eefa1c88a0425bce22935ee5499cc7d33a6c4d5
aymeentestdev <aymen.ennaji.dev@gmail.com>
2025-06-11 13:37:01 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,15 +38,16 @@ const Footer: React.FC = () => {
             <ul className="space-y-3">
               <li className="flex items-start space-x-3">
                 <MapPin className="h-5 w-5 text-secondary-400 flex-shrink-0 mt-0.5" />
-                <span className="text-gray-300">123 Avenue de l'Éducation, Casablanca, Maroc</span>
+                <span className="text-gray-300">Genial Office, 2eme etage  B7, Avenue Mohamed Taieb
+                  Naciri, à côté de CFC, Casablanca 20180</span>
               </li>
               <li className="flex items-center space-x-3">
                 <Phone className="h-5 w-5 text-secondary-400 flex-shrink-0" />
-                <span className="text-gray-300">+212 5XX-XXXXXX</span>
+                <span className="text-gray-300">+212 684 475 639</span>
               </li>
               <li className="flex items-center space-x-3">
                 <Mail className="h-5 w-5 text-secondary-400 flex-shrink-0" />
-                <span className="text-gray-300">contact@iaai-edu.ma</span>
+                <span className="text-gray-300">contact.iaaiac@gmail.com</span>
               </li>
             </ul>
           </div>
